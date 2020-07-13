e710476bdd0dd33ed406023cc5ecfe48fc4f4049
tnt <kientuong2000@gmail.com>
2020-07-13 21:14:43 +0700

    delete object's xpath

--- Object Repository/Page_Google/input_Xa_btnI_feelinglucky.rs
+++ Object Repository/Page_Google/input_Xa_btnI_feelinglucky.rs
@@ -75,25 +75,4 @@
       <type>Main</type>
       <value>id(&quot;tsf&quot;)/div[2]/div[@class=&quot;A8SBwf&quot;]/div[@class=&quot;FPdoLc tfB0Bf&quot;]/center[1]/input[@class=&quot;RNmpXc&quot;]</value>
    </webElementProperties>
-   <webElementXpaths>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:attributes</name>
-      <type>Main</type>
-      <value>(//input[@name='btnI'])[2]</value>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:idRelative</name>
-      <type>Main</type>
-      <value>//form[@id='tsf']/div[2]/div/div[3]/center/input[2]</value>
-   </webElementXpaths>
-   <webElementXpaths>
-      <isSelected>false</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>xpath:position</name>
-      <type>Main</type>
-      <value>//div[3]/center/input[2]</value>
-   </webElementXpaths>
 </WebElementEntity>
